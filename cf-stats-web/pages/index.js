fa687643c80a29913a26e2606e27ae07b7b7a947
Mauricio <mcimauricio.coelho@gmail.com>
2021-01-22 18:39:29 +0100

    Add search and name

--- cf-stats-web/pages/index.js
+++ cf-stats-web/pages/index.js
@@ -1,16 +1,21 @@
 import Head from "next/head";
 import "tailwindcss/tailwind.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-x-0.5 flex flex-col justify-center items-center">
+    <div className="min-h-screen p-x-0.5 flex flex-col">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="p-x-0.5 flex-col justify-center items-center bg-blue-500">
-        <h1>Hello</h1>
+      <main className="flex flex-1 flex-col w-full pt-10 items-center">
+        <h1 className="text-6xl py-2">Crossfit Stats</h1>
+        <div className="w-1/2 flex border border-gray-200 rounded-full p-4 shadow text-xl">
+          <div>🕵</div>
+          <input type="text" className="w-full outline-none px-3" />
+        </div>
       </main>
     </div>
   );
